03ef3443e1e0ebdcd464c748e95776f38efed99a
Arasy Dafa Sulistya Kurniawan <arasy.dafa@gmail.com>
2021-07-28 20:16:52 +0700

    Upload ke github

---
+++ Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace Ultrasonic_Motion_Detector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPort = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.motionBox = new System.Windows.Forms.RichTextBox();
+            this.statusBox = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelWaktu = new System.Windows.Forms.Label();
+            this.labelTanggal = new System.Windows.Forms.Label();
+            this.motionLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(154)))));
+            this.comboBoxPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold);
+            this.comboBoxPort.ForeColor = System.Drawing.Color.White;
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(254, 209);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(170, 39);
+            this.comboBoxPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(154)))));
+            this.comboBoxBaud.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold);
+            this.comboBoxBaud.ForeColor = System.Drawing.Color.White;
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "74880",
+            "115200"});
+            this.comboBoxBaud.Location = new System.Drawing.Point(254, 273);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(170, 39);
+            this.comboBoxBaud.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(66, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baud Rate";
+            // 
+            // buttonPort
+            // 
+            this.buttonPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(74)))), ((int)(((byte)(154)))));
+            this.buttonPort.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPort.ForeColor = System.Drawing.Color.White;
+            this.buttonPort.Location = new System.Drawing.Point(110, 370);
+            this.buttonPort.Name = "buttonPort";
+            this.buttonPort.Size = new System.Drawing.Size(248, 59);
+            this.buttonPort.TabIndex = 2;
+            this.buttonPort.Text = "Connect";
+            this.buttonPort.UseVisualStyleBackColor = false;
+            this.buttonPort.Click += new System.EventHandler(this.buttonPort_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(727, 184);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(597, 203);
+            this.listBox1.TabIndex = 3;
+            // 
+            // motionBox
+            // 
+            this.motionBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.motionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.motionBox.Location = new System.Drawing.Point(1039, 457);
+            this.motionBox.Name = "motionBox";
+            this.motionBox.ReadOnly = true;
+            this.motionBox.Size = new System.Drawing.Size(274, 31);
+            this.motionBox.TabIndex = 4;
+            this.motionBox.Text = "";
+            // 
+            // statusBox
+            // 
+            this.statusBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.statusBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.statusBox.Location = new System.Drawing.Point(723, 457);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.ReadOnly = true;
+            this.statusBox.Size = new System.Drawing.Size(291, 31);
+            this.statusBox.TabIndex = 4;
+            this.statusBox.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 686);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1348, 35);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel1.ForeColor = System.Drawing.Color.White;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(327, 32);
+            this.toolStripLabel1.Text = "Please Connect a COM Port";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelWaktu
+            // 
+            this.labelWaktu.AutoSize = true;
+            this.labelWaktu.BackColor = System.Drawing.Color.Transparent;
+            this.labelWaktu.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWaktu.ForeColor = System.Drawing.Color.White;
+            this.labelWaktu.Location = new System.Drawing.Point(445, 9);
+            this.labelWaktu.Name = "labelWaktu";
+            this.labelWaktu.Size = new System.Drawing.Size(239, 79);
+            this.labelWaktu.TabIndex = 6;
+            this.labelWaktu.Text = "Waktu";
+            // 
+            // labelTanggal
+            // 
+            this.labelTanggal.AutoSize = true;
+            this.labelTanggal.BackColor = System.Drawing.Color.Transparent;
+            this.labelTanggal.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTanggal.ForeColor = System.Drawing.Color.White;
+            this.labelTanggal.Location = new System.Drawing.Point(452, 88);
+            this.labelTanggal.Name = "labelTanggal";
+            this.labelTanggal.Size = new System.Drawing.Size(170, 46);
+            this.labelTanggal.TabIndex = 6;
+            this.labelTanggal.Text = "Tanggal";
+            // 
+            // motionLabel
+            // 
+            this.motionLabel.AutoSize = true;
+            this.motionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.motionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.motionLabel.ForeColor = System.Drawing.Color.White;
+            this.motionLabel.Location = new System.Drawing.Point(937, 151);
+            this.motionLabel.Name = "motionLabel";
+            this.motionLabel.Size = new System.Drawing.Size(205, 32);
+            this.motionLabel.TabIndex = 1;
+            this.motionLabel.Text = "Serial Monitor";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(826, 425);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(102, 32);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Status";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox.Image = global::Ultrasonic_Motion_Detector.Properties.Resources.Standing_Man;
+            this.pictureBox.Location = new System.Drawing.Point(476, 151);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(162, 337);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Ultrasonic_Motion_Detector.Properties.Resources.Fench;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 516);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Ultrasonic_Motion_Detector.Properties.Resources.Fench;
+            this.pictureBox2.Location = new System.Drawing.Point(451, 516);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(440, 134);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Ultrasonic_Motion_Detector.Properties.Resources.Fench;
+            this.pictureBox3.Location = new System.Drawing.Point(897, 516);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(440, 134);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(1130, 425);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Motion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.labelTanggal);
+            this.Controls.Add(this.labelWaktu);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.motionBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.motionLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxBaud);
+            this.Controls.Add(this.comboBoxPort);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ultrasonic Motion Detector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonPort;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox motionBox;
+        private System.Windows.Forms.RichTextBox statusBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelWaktu;
+        private System.Windows.Forms.Label labelTanggal;
+        private System.Windows.Forms.Label motionLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using Bulb;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ultrasonic_Motion_Detector
+{
+    public partial class Form1 : Form
+    {
+        string motion;
+        string status;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = "Ultrasonic Motion Detector v" + Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] port = SerialPort.GetPortNames();
+                foreach (string name in port)
+                {
+                    comboBoxPort.Items.Add(name);
+                }
+                comboBoxPort.Text = comboBoxPort.Items[comboBoxPort.Items.Count - 1].ToString();
+                comboBoxBaud.Text = comboBoxBaud.Items[comboBoxBaud.Items.Count - 1].ToString();
+
+                labelTanggal.Text = DateTime.Now.ToString("dddd, dd MMMM yyyy", new CultureInfo("id-ID"));
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String data = serialPort.ReadLine();
+            showData(data);
+        }
+
+        private delegate void showDataDelegate(object item);
+
+        private void showData(object item)
+        {
+            if (InvokeRequired)
+            {
+                listBox1.Invoke(new showDataDelegate(showData), item);
+            }
+            else
+            { 
+                listBox1.Items.Add(item);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                splitData(item);
+            }
+        }
+
+        private void splitData(object item)
+        {
+            string[] data = item.ToString().Split(',');
+
+            motion = data[0];
+            motionBox.Text = motion;
+            status = data[1];
+            statusBox.Text = status;
+
+            if (motion == "MotionDetected" && status == "Caution")
+            {
+                motionBox.ForeColor = Color.White;
+                motionBox.BackColor = Color.Red;
+
+                statusBox.ForeColor = Color.Black;
+                statusBox.BackColor = Color.Yellow;
+
+                pictureBox.Image = Properties.Resources.Walking_Man;
+            }
+            else if (motion == "NoMotionDetected" && status == "Clear")
+            {
+                motionBox.ForeColor = Color.White;
+                motionBox.BackColor = Color.Green;
+
+                statusBox.ForeColor = Color.White;
+                statusBox.BackColor = Color.Green;
+
+                pictureBox.Image = Properties.Resources.Standing_Man;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelWaktu.Text = DateTime.Now.ToString("hh : mm : ss");
+        }
+
+        private void buttonPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort.IsOpen == false)
+                {
+                    buttonPort.Text = "Disconnect";
+                    serialPort.PortName = comboBoxPort.Text;
+                    serialPort.BaudRate = Int32.Parse(comboBoxBaud.Text);
+                    serialPort.NewLine = "\n\r";
+                    serialPort.Open();
+                    toolStripLabel1.Text = "Connected to " + serialPort.PortName;
+                }
+                else if (serialPort.IsOpen == true)
+                {
+                    buttonPort.Text = "Connect";
+                    serialPort.Close();
+                    toolStripLabel1.Text = "Disconnected from " + serialPort.PortName;
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripLabel1.Text = "ERROR : " + ex.Message.ToString();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.FromArgb(221, 56, 87), 1);
+
+            Rectangle area = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+            LinearGradientBrush brush = new LinearGradientBrush(area, Color.FromArgb(234, 203, 198), 
+                                                                      Color.FromArgb(221, 56, 87),
+                                                                      LinearGradientMode.Vertical);
+            graphics.FillRectangle(brush, area);
+            graphics.DrawRectangle(pen, area);
+        }
+    }
+}
